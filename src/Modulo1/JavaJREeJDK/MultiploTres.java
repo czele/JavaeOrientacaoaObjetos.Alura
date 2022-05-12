d379eb0d323327136e4368064db7a99a46c83d2a
czele <camila_zele@hotmail.com>
2022-05-12 17:00:28 -0300

    Exercício Alíquota IR resolvido

---
+++ src/Modulo1/JavaJREeJDK/MultiploTres.java
@@ -0,0 +1,9 @@
+package Modulo1.JavaJREeJDK;
+
+//Utilize um laço do tipo for para imprimir todos os múltiplos de 3, entre 1 e 100.
+public class MultiploTres {
+    public static void main(String[] args) {
+
+
+    }
+}
