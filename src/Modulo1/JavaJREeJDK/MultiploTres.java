1c40793780ae94446786bb0f20c56c348f76131a
czele <camila_zele@hotmail.com>
2022-05-13 14:57:37 -0300

    Exercício MultiploTres resolvido

--- src/Modulo1/JavaJREeJDK/MultiploTres.java
+++ src/Modulo1/JavaJREeJDK/MultiploTres.java
@@ -3,7 +3,8 @@ package Modulo1.JavaJREeJDK;
 //Utilize um laço do tipo for para imprimir todos os múltiplos de 3, entre 1 e 100.
 public class MultiploTres {
     public static void main(String[] args) {
-
-
+        for (int multiploTres = 1; multiploTres <= 100; multiploTres++) {
+            System.out.println(multiploTres * 3);
+        }
     }
 }
